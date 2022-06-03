801a9fe5a188ae87890108d91be183e9e598210c
Germán <pequerag@gmail.com>
2022-06-03 16:32:31 -0300

    Calses

--- ejercicios/05_clases/01_clase.dart
+++ ejercicios/05_clases/01_clase.dart
@@ -2,14 +2,21 @@ import 'clases/persona.dart';
 
 main() {  
 
-  final Persona persona = new Persona();
+  final Persona persona = new Persona(edad: 36,nombre: 'German');
+  final Persona persona2 = new Persona.persona30('German');
+  final Persona persona3 = new Persona.persona40('German');
+  final Persona persona4 = new Persona.nombreDefault(36);
 
-  persona..nombre = 'German'
-         ..edad   = 36
-         ..bio    = 'algo en la bio';
+
+  // persona..nombre = 'German'
+  //        ..edad   = 36;
+         //..bio    = 'algo en la bio';
 
 
   print(persona);
+  print(persona2);
+  print(persona3);
+  print(persona4);
 }
 
 
--- ejercicios/05_clases/clases/persona.dart
+++ ejercicios/05_clases/clases/persona.dart
@@ -20,8 +20,29 @@ String get bio => _bio; //funcion de flecha
 set bio( String texto) => _bio = texto; //funcion de flecha 
 
 
-// Constructores 
+// Constructores
+// Persona( int edad, String nombre){
 
+//   this.edad = edad;
+//   this.nombre = nombre;
+
+// } 
+  Persona({this.edad, this.nombre}); //contructor mas resumido que el de arriba
+//{} hace los argumumentos con nombre y opcionales
+
+  Persona.persona30(this.nombre){
+    this.edad = 30;
+  }
+  
+  Persona.persona40(String nombre){
+    this.nombre = nombre;
+    this.edad = 40;
+  }
+
+  Persona.nombreDefault(int edad){
+    this.edad = edad;
+    this.nombre = 'Sin nombre';
+  }
 // Metodos
 @override
   String toString() => '$nombre $edad $_bio';
