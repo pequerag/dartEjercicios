99decddff13df7e52dd4280ccd1ffa689a1bf695
Germán <pequerag@gmail.com>
2022-06-28 20:23:35 -0300

    Peticiones http

--- paquetes/bin/paquetes.dart
+++
@@ -1,5 +0,0 @@
-import 'package:paquetes/paquetes.dart' as paquetes;
-
-void main(List<String> arguments) {
-  print('Hello world: ${paquetes.calculate()}!');
-}
--- paquetes/test/paquetes_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:paquetes/paquetes.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
